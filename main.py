c9615287e17dd35549fc4d247ebfaf5cd88dcfe3
Steven Tompkins <steven.tompkins.jr@gmail.com>
2019-09-22 07:53:58 -0500

    Multithreaded extraction + dry run

--- main.py
+++ main.py
@@ -2,32 +2,49 @@ import argparse
 import os
 import zipfile
 from concurrent.futures import ThreadPoolExecutor, wait
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('extractor')
 
 
-def extract(entry: os.DirEntry, destination: str):
+def extract(entry: os.DirEntry, destination: str, dry_run: bool):
     if '.zip' in entry.name:
-        print("unzipping '{}'".format(entry.name))
+        logger.info("unzipping '{}'".format(entry.name))
         zipped = zipfile.ZipFile(entry.path)
-        zipped.extractall(path=destination)
-        print("unzipped '{}'".format(entry.name))
+        for name in zipped.namelist():
+            if '.zip' in name:
+                logger.debug("unzipping internal entries...")
+                tmp = os.path.join(entry.name, destination)
+                if not dry_run:
+                    zipped.extractall(path=tmp)
+                # os.remove(entry.path)
+                for entry in os.scandir(tmp):
+                    extract(entry, destination, dry_run)
+            else:
+                if not dry_run:
+                    zipped.extractall(path=destination)
+        logger.info("unzipped '{}'".format(entry.name))
     else:
-        print("skipping '{}'".format(entry.name))
+        logger.info("skipping '{}'".format(entry.name))
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="the directory of the zip files", type=str, required=True)
     parser.add_argument("-o", "--output", help="the directory to unzip everything to", type=str,  default="./unzipped")
+    parser.add_argument("--dry_run", help="print out what would be done, but don't actualy unzip", type=bool, default=False)
     args = parser.parse_args()
 
     source = args.input
     destination = args.output
+    dry_run = args.dry_run
 
     submissions = {}
     executor = ThreadPoolExecutor()
 
     for entry in os.scandir(source):
-        submissions[executor.submit(extract, entry, destination)] = entry.name
+        submissions[executor.submit(extract, entry, destination, dry_run)] = entry.name
 
     _, _ = wait(submissions)
 
